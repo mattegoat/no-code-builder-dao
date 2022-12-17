00b0975f0e9125e30ab1048b7598647375f879d4
Mattegoat <matteo.georges74@hotmail.fr>
2022-12-17 21:28:38 +0100

    feat: proposal page

--- pages/vote/[pid].tsx
+++ pages/vote/[pid].tsx
@@ -71,6 +71,88 @@ const ProposalPage: NextPage = () => {
       <div className={classes.markdown}>
         {ReactHtmlParser(proposal?.description.split('&&')[1] || '')}
       </div>
+      <div></div>
+      <h1 className="font-bold text-xl py-5 text-primary">Proposed transactions</h1>
+      {/* TODO PROPOSED TX */}
+      <h1 className="font-bold text-xl py-5 text-primary">Voting Stats</h1>
+
+      <div className="flex flex-row gap-4 mb-4">
+        <div className="flex flex-col gap-4 p-4 outline w-1/3 bg-neutral card">
+          <h1 className="text-success text-center font-bold text-xl pb-6">For</h1>
+          <h1 className=" text-center font-bold text-3xl pb-3">10</h1>
+          <progress
+            className="progress progress-success w-full mt-3"
+            value="10"
+            max="100"></progress>
+        </div>
+        <div className="flex flex-col gap-4 p-4 outline w-1/3 bg-neutral card">
+          <h1 className="text-error text-center font-bold text-xl pb-6">Against</h1>
+          <h1 className=" text-center font-bold text-3xl pb-3">10</h1>
+          <progress
+            className="progress progress-error w-full mt-3"
+            value="10"
+            max="100"></progress>
+        </div>
+        <div className="flex flex-col gap-4 p-4 outline w-1/3 bg-neutral card">
+          <h1 className="text-info text-center font-bold text-xl pb-6">Abstain</h1>
+          <h1 className=" text-center font-bold text-3xl pb-3">10</h1>
+          <progress
+            className="progress progress-info w-full mt-3"
+            value="10"
+            max="100"></progress>
+        </div>
+      </div>
+      <div className="flex flex-row gap-4 mb-4">
+        <div className="flex flex-col gap-4 px-10 py-4 outline w-1/3 bg-neutral card">
+          <h1 className=" font-bold text-xl pb-2">Voters</h1>
+          <h1 className=" text-right font-bold text-3xl text-secondary mt-auto pb-4">
+            10 <span className="text-sm text-primary-content">/40</span>
+          </h1>
+        </div>
+        <div className="flex flex-col gap-4 px-10 py-4 outline w-1/3 bg-neutral card">
+          <h1 className="font-bold text-xl pb-2">Threshold</h1>
+          <h1 className=" text-right font-bold text-3xl text-secondary mt-auto pb-4">
+            20
+          </h1>
+        </div>
+        <div className="flex flex-col gap-4 px-10 py-4 outline w-1/3 bg-neutral card">
+          <h1 className="font-bold text-xl pb-2">Ends</h1>
+          <h1 className=" text-right font-bold text-3xl text-secondary mt-auto pb-4">
+            1/12/1030
+          </h1>
+        </div>
+        <div className="flex flex-col gap-4 px-10 py-4 outline w-1/3 bg-neutral card">
+          <h1 className="font-bold text-xl pb-2">% of Treasury</h1>
+          <div
+            className="radial-progress text-secondary h-13 w-13 ml-auto"
+            style={{ '--value': 70 } as React.CSSProperties}>
+            70%
+          </div>
+        </div>
+      </div>
+      <label htmlFor="my-modal-4" className="btn outline w-full mb-6">
+        Vote Now
+      </label>
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <h3 className="text-lg font-bold">Vote</h3>
+          <p className="py-4">You have x votes</p>
+          <div className="w-full flex flex-row gap-3">
+            <select className="select select-primary w-full max-w-xs">
+              <option disabled selected>
+                Choose the option
+              </option>
+              <option>For</option>
+              <option>Against</option>
+              <option>Abstain</option>
+            </select>
+          </div>
+          <div className="modal-action">
+            <a className="btn">Vote</a>
+          </div>
+        </label>
+      </label>
     </div>
   )
 }
