5d5c2a64a12938b1f6326832ed0e9b46494f212e
Mattegoat <matteo.georges74@hotmail.fr>
2022-12-18 20:21:02 +0100

    feat: markdown renderer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import { Seo } from 'components'
 import dynamic from 'next/dynamic'
 import useSWR from 'swr'
 import { useEffect } from 'react'
+import classes from './styles.module.css'
+import ReactMarkdown from 'react-markdown'
+import remarkBreaks from 'remark-breaks'
+
 /*
 const CurrentAuction = dynamic(() => import('../@dao-auction/components/CurrentAuction'), {
   ssr: false,
@@ -16,6 +20,14 @@ const fetcher = (url: any) => fetch(url).then((res) => res.json())
 
 const Home: NextPage = () => {
   const { data, error } = useSWR('/api/staticdata', fetcher)
+
+  const EditerMarkdown = dynamic(
+    () =>
+      import('@uiw/react-md-editor').then((mod) => {
+        return mod.default.Markdown
+      }),
+    { ssr: false }
+  )
 
   useEffect(() => {
     console.log(data)
@@ -33,7 +45,12 @@ const Home: NextPage = () => {
         <TokenExplorer daoAddress="0xd2E7684Cf3E2511cc3B4538bB2885Dc206583076" />
       </section>
       <section id="custom-content" className="pt-10">
-        <div className="text-black">{data?.markdown}</div>
+        <div className={classes.markdown}>
+          {/* <ReactMarkdown remarkPlugins={[remarkBreaks]}>
+            {data?.markdown.replace(/\n/gi, '&nbsp; \n')}
+          </ReactMarkdown> */}
+          <EditerMarkdown className={classes.markdown} source={data?.markdown} />
+        </div>
       </section>
     </>
   )
