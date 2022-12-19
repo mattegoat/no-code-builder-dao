eb0b0b05e9fba6d109f39add4eb50e0c34fd69ef
Evolve <Artevolve@yahoo.com>
2022-12-19 18:34:03 +0100

    fix: temp remove seo vars ✍️

--- utils/env-vars.ts
+++ utils/env-vars.ts
@@ -3,11 +3,11 @@ import Config from '../config/config.json'
 /* 
   SEO
 **/
-export const APP_TITLE = Config.SITE_TITLE
-export const APP_DESCRIPTION = Config.SITE_DESCRIPTION || ''
-export const BASE_URL = Config.WEBSITE_URL || ''
-export const OG_IMAGE = `${BASE_URL}/meta-content/og-image.jpg`
-export const FAVICON = `${BASE_URL}/meta-content/favicon.png`
+// export const APP_TITLE = Config.SITE_TITLE
+// export const APP_DESCRIPTION = Config.SITE_DESCRIPTION || ''
+// export const BASE_URL = Config.WEBSITE_URL || ''
+// export const OG_IMAGE = `${BASE_URL}/meta-content/og-image.jpg`
+// export const FAVICON = `${BASE_URL}/meta-content/favicon.png`
 export const TWITTER_HANDLE = Config.twitter
 export const DISCORD_LINK = Config.discord
 export const LOGO_URL = Config.logo
